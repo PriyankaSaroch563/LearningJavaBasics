1400bdb04bd3efb1725d770fccc7dc33046fa563
PriyankaSaroch <priyanka@upperthrust.com>
2025-05-19 20:13:31 +0530

    19th May 2025

--- src/ex_01_JavaBasics/Lab010_MultipleMain.java
+++ src/ex_01_JavaBasics/Lab010_MultipleMain.java
@@ -20,6 +20,12 @@ public class Lab010_MultipleMain {
      * JVM should be present.
      * The concept of multiple main function in the program is called as main method overloading.
      * > Functions == Methods(class) in Java
+     *
+     * #### Can we run a program without a main method?
+     * 1. Java < 7 - Yes ( if yes static block)
+     * 2. 8 to 21 JDK - it was not possible
+     * 3. 21-> there is way.  (static)
+
      */
 
 }
